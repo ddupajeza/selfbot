0e2271c952fde650942e66ddd75398095b946af0
Deadpoker <ArturKayl@yahoo.de>
2017-09-03 16:13:24 +0200

    Update my.py

--- cogs/my.py
+++ cogs/my.py
@@ -1,1 +1,10 @@
-placeholder
+import discord
+from discord.ext import commands
+
+class my:
+    def __init__(self, bot):
+        self.bot = bot
+        self.sessions = set()
+        
+  @command.command()
+  print(5+3)
